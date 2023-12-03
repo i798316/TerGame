f4ad00dff9644c3c9f45add8405a8b7e9a08b415
i798316 <798316@unizar.es>
2023-12-03 22:32:01 +0100

    Ingeniería de requisitos, toma de contacto con el proyecto

---
+++ TerGame/resources/views/layouts/template.blade.php
@@ -0,0 +1,11 @@
+<!-- layouts/template.blade.php (Vista Principal) -->
+<html>
+    <head>
+        <!-- Cabecera -->
+        <title>TerGame</title>
+    </head>
+    <body>
+        @yield('content')
+        @include('partials.footer')
+    </body>
+</html>
---
+++ TerGame/resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<!-- partials/footer.blade.php (Vista Parcial) -->
+<footer>
+    <!-- Pie de Página -->
+    <p> Pie de página</p>
+</footer>
